e5ea9d47371788273b3e3b5141e2bc1a8751e08b
MinhNguyenETH <minhnguyen.eth@gmail.com>
2025-06-11 08:41:30 +0700

    push debt

--- pages/DebtPage.ts
+++ pages/DebtPage.ts
@@ -1,35 +1,224 @@
-import { Page, Locator, expect } from '@playwright/test';
+import { Page, Locator, expect } from "@playwright/test";
 export class DebtPage {
     private page: Page;
 
     readonly toastAddSuccess: Locator;
     readonly toastUpdateSuccess: Locator;
     readonly toastDeleteSuccess: Locator;
+    readonly toastCancelSuccess: Locator;
+    readonly toastSendSuccess: Locator;
+    readonly toastBrowserSuccess: Locator;
+    readonly toastRefusedSuccess: Locator;
     readonly debtButton: Locator;
-
+    readonly addButton: Locator;
+    readonly editButton: Locator;
+    readonly deleteButton: Locator;
+    readonly searchNameInput: Locator;
+    readonly chosesearchName: Locator;
+    readonly searchButton: Locator;
+    readonly saveButton: Locator;
+    readonly cancelButton: Locator;
+    readonly requiredErrorNameMessage: Locator;
+    readonly requiredErrorAmountMessage: Locator;
+    readonly requiredErrorNoteMessage: Locator;
+    readonly requiredErrorReasonMessage: Locator;
+    readonly inputName: Locator;
+    readonly inputAmount: Locator;
+    readonly inputNote: Locator;
+    readonly chosenName: Locator;
+    readonly actionButton: Locator;
+    readonly actionCancelButton: Locator;
+    readonly reasonInput: Locator;
+    readonly yesButton: Locator;
+    readonly noButton: Locator;
+    readonly actionSendButton: Locator;
+    readonly logoutButton: Locator;
+    readonly actionBrowserButton: Locator;
+    readonly actionRefusedButton: Locator;
 
     constructor(page: Page) {
         this.page = page;
 
         this.debtButton = page.locator("//div[contains(text(),'Tạm ứng')]");
-        this.toastAddSuccess = page.locator('//div[contains(text(),"Thêm thành công")]');
-        this.toastUpdateSuccess = page.locator('//div[contains(text(),"Cập nhật thành công")]');
-        this.toastDeleteSuccess = page.locator('//div[contains(text(),"Xóa thành công")]');
+        this.toastAddSuccess = page.locator(
+            '//div[contains(text(),"Thêm thành công")]'
+        );
+        this.toastUpdateSuccess = page.locator(
+            '//div[contains(text(),"Cập nhật thành công")]'
+        );
+        this.toastDeleteSuccess = page.locator(
+            '//div[contains(text(),"Xóa thành công")]'
+        );
+        this.toastCancelSuccess = page.locator(
+            '//div[contains(text(),"Hủy thành công")]'
+        );
+        this.toastSendSuccess = page.locator(
+            '//div[contains(text(),"Đã gửi thành công")]'
+        );
+        this.toastBrowserSuccess = page.locator(
+            '//div[contains(text(),"Đã gửi thành công")]'
+        );
+        this.toastRefusedSuccess = page.locator(
+            '//div[contains(text(),"Từ chối thành công")]'
+        );
+        this.addButton = page.locator("//span[normalize-space()='Thêm']");
+        this.searchButton = page.locator("//span[normalize-space()='Tìm kiếm']");
+        this.saveButton = page.locator("//span[normalize-space()='Lưu']");
+        this.cancelButton = page.locator("//span[normalize-space()='Hủy']");
+        this.requiredErrorNameMessage = page.getByText("Nhập chọn nhân viên");
+        this.requiredErrorAmountMessage = page.getByText("Nhập nhập số tiền");
+        this.requiredErrorNoteMessage = page.getByText("Nhập ghi chú");
+        this.requiredErrorReasonMessage = page.getByText("Nhập lý do");
+        this.inputName = page.getByRole("textbox", {
+            name: "Chọn nhân viên ※ Chọn nhân vi",
+        });
+        this.chosenName = page.getByText("BAT810-Nguyễn Văn Minh");
+        this.inputAmount = page.getByRole('textbox', { name: 'Nhập số tiền ※ Nhập số tiền ※' })
+        this.inputNote = page.getByRole("textbox", { name: "Ghi chú ※ Ghi chú ※" });
+        this.editButton = page.getByRole('button', { name: 'Sửa' });
+        this.searchNameInput = page.getByRole('textbox', { name: 'Tên nhân viên Tên nhân viên' });
+        this.chosesearchName = page.getByRole('option', { name: 'BAT810 - Nguyễn Văn Minh' });
+        this.actionCancelButton = page.getByRole('button', { name: 'Hủy' });
+        this.reasonInput = page.getByRole('textbox', { name: 'Lý do ※ Lý do ※' })
+        this.yesButton = page.getByRole('button', { name: 'Có' });
+        this.noButton = page.getByRole('button', { name: 'Không' });
+        this.actionSendButton = page.getByRole('button', { name: 'Gửi' });
+        this.logoutButton = page.locator("//span[normalize-space()='Đăng xuất']");
+        this.actionBrowserButton = page.getByRole('button', { name: 'Duyệt' });
+        this.actionRefusedButton = page.getByRole('button', { name: 'Từ chối' });
+    }
 
+    // Hàm xử lý
+
+
+
+    async getToastAddSuccess() {
+        await expect(this.toastAddSuccess).toHaveText("Thêm thành công");
+        return this.toastAddSuccess.textContent();
+    }
+
+    async clickAddButton() {
+        await this.addButton.click();
     }
 
 
+    async clickSaveButton() {
+        await this.saveButton.click();
+    }
 
-    // Hàm xử lý
+    async clickCancelButton() {
+        await this.cancelButton.click();
+    }
+
+    async expectFillNameError() {
+        await expect(this.requiredErrorNameMessage).toHaveText(
+            "Nhập chọn nhân viên"
+        );
+    }
+    
     async clickDebtButton() {
         await this.debtButton.click();
     }
 
-    async getToastAddSuccess() {
-        await expect(this.toastAddSuccess).toHaveText('Thêm thành công');
-        return this.toastAddSuccess.textContent();
+
+    async expectFillNoteError() {
+        await expect(this.requiredErrorNoteMessage).toHaveText("Nhập ghi chú");
     }
 
+    async fillName(name: string) {
+        await this.inputName.fill(name);
+        await this.chosenName.click();
+    }
 
+    async fillAmount(amount: string) {
+        await this.inputAmount.fill(amount);
+    }
+
+    async fillNote(note: string) {
+        await this.inputNote.fill(note);
+    }
+
+    async clickEditButton() {
+        await this.searchNameInput.fill('BAT810 - Nguyễn Văn Minh');
+        await this.chosesearchName.click();
+        await this.searchButton.click();
+        await this.page.getByRole('row', { name: '1 BAT810 - Nguyễn Văn Minh 1.000.000 đ add debt test for edit Admin Tạo mới', exact: true }).getByRole('button').click();
+        await this.editButton.click();
+    }
+
+    async getToastEditSuccess() {
+        await expect(this.toastUpdateSuccess).toHaveText("Cập nhật thành công");
+        return this.toastUpdateSuccess.textContent();
+    }
+
+    async expectFillAmountError() {
+        await expect(this.requiredErrorAmountMessage).toHaveText("Nhập nhập số tiền");
+    }
+
+    async clickActionCancelButton() {
+        await this.searchNameInput.fill('BAT810 - Nguyễn Văn Minh');
+        await this.chosesearchName.click();
+        await this.searchButton.click();
+        await this.page.getByRole('row', { name: '1 BAT810 - Nguyễn Văn Minh 1.000.000 đ add debt test for cancel Admin Tạo mới', exact: true }).getByRole('button').click();
+        await this.actionCancelButton.click();
+    }
+
+    async getToastCancelSuccess() {
+        await expect(this.toastCancelSuccess).toHaveText("Hủy thành công");
+        return this.toastCancelSuccess.textContent();
+    }
+
+    async fillReason(reason: string) {
+        await this.reasonInput.fill(reason);
+    }
+
+    async clickYesButton() {
+        await this.yesButton.click();
+    }
+
+    async clickActionSendButton() {
+        await this.searchNameInput.fill('BAT810 - Nguyễn Văn Minh');
+        await this.chosesearchName.click();
+        await this.searchButton.click();
+        await this.page.getByRole('row', { name: '1 BAT810 - Nguyễn Văn Minh 1.000.000 đ add debt test for send Admin Tạo mới', exact: true }).getByRole('button').click();
+        await this.actionSendButton.click();
+    }
+
+    async expectFillReasonError() {
+        await expect(this.requiredErrorReasonMessage).toHaveText("Nhập lý do");
+    }
+
+    async getToastSendSuccess() {
+        await expect(this.toastSendSuccess).toHaveText("Đã gửi thành công");
+        return this.toastSendSuccess.textContent();
+    }
+
+    async clickLogoutButton() {
+        await this.logoutButton.click();
+    }
+
+    async clickActionBrowsedButton() {
+        await this.page.getByRole('row', { name: '1 BAT810 - Nguyễn Văn Minh 1.000.000 đ add debt test for send Admin Đã gửi', exact: true }).getByRole('button').click();
+        await this.actionBrowserButton.click();
+    }
+
+    async getToastBrowseSuccess() {
+        await expect(this.toastBrowserSuccess).toHaveText("Đã gửi thành công");
+        return this.toastBrowserSuccess.textContent();
+    }
+
+    async clickActionRefusedButton() {
+        await this.page.getByRole('row', { name: '1 BAT810 - Nguyễn Văn Minh 1.000.000 đ add debt test for send Admin Đã gửi', exact: true }).getByRole('button').click();
+        await this.actionRefusedButton.click();
+    }
+
+    async getToastRefusedSuccess() {
+        await expect(this.toastRefusedSuccess).toHaveText("Từ chối thành công");
+        return this.toastRefusedSuccess.textContent();
+    }
+
+    async clickActionSendCancelButton() {
+        await this.page.getByRole('row', { name: '1 BAT810 - Nguyễn Văn Minh 1.000.000 đ add debt test for send Admin Đã gửi', exact: true }).getByRole('button').click();
+        await this.actionCancelButton.click();
+    }
 }
-
--- tests/debt.spec.ts
+++ tests/debt.spec.ts
@@ -1,40 +1,279 @@
-import { test, expect, Page, TestInfo } from '@playwright/test';
-import { LoginPage } from '../pages/LoginPage';
-import { takeScreenshotOnFailure } from '../utils/screenshotUtils';
-import Config from '../utils/configUtils';
-import { HomePage } from '../pages/HomePage';
-import { ToastPage } from '../pages/ToastPage';
-import { DebtPage } from '../pages/DebtPage';
 
+import { test, expect, Page, TestInfo } from "@playwright/test";
+import { LoginPage } from "../pages/LoginPage";
+import { takeScreenshotOnFailure } from "../utils/screenshotUtils";
+import Config from "../utils/configUtils";
+import { HomePage } from "../pages/HomePage";
+import { ToastPage } from "../pages/ToastPage";
+import { DebtPage } from "../pages/DebtPage";
+import { throws } from "assert";
 
+test.describe.serial("Debt Tests", () => {
+  let loginPage: LoginPage;
+  let debtPage: DebtPage;
+  let homePage: HomePage;
+  let toastPage: ToastPage;
 
-test.describe.serial('Debt Tests', () => {
-    let loginPage: LoginPage;
-    let debtPage: DebtPage;
-    let homePage: HomePage;
-    let toastPage: ToastPage;
+  const randomSuffix = Math.random().toString(36).substring(2, 8);
 
-    const randomSuffix = Math.random().toString(36).substring(2, 8);
-    
+  test.beforeEach(async ({ page }) => {
+    loginPage = new LoginPage(page);
+    debtPage = new DebtPage(page);
+    homePage = new HomePage(page);
+    toastPage = new ToastPage(page);
+    await loginPage.goto();
+  });
 
-    test.beforeEach(async ({ page }) => {
-        loginPage = new LoginPage(page);
-        debtPage = new DebtPage(page)
-        homePage = new HomePage(page);
-        toastPage = new ToastPage(page);
-        await loginPage.goto();
+  test.afterEach(async ({ page }, testInfo: TestInfo) => {
+    await takeScreenshotOnFailure(page, testInfo);
+  });
+
+  test("empty value Test", async ({ page }) => {
+    await loginPage.login(Config.admin_username, Config.admin_password);
+    await homePage.clickSalary();
+    await debtPage.clickDebtButton();
+    await debtPage.clickAddButton();
+    await debtPage.clickSaveButton();
+    await debtPage.expectFillNameError();
+    await debtPage.expectFillNoteError();
+  });
+
+  test("add debt Test", async ({ page }) => {
+    await loginPage.login(Config.admin_username, Config.admin_password);
+    await homePage.clickSalary();
+    await debtPage.clickDebtButton();
+    await debtPage.clickAddButton();
+    await debtPage.fillName("BAT810-Nguyễn Văn Minh");
+    await debtPage.fillAmount("1000000");
+    await debtPage.fillNote("add debt test ");
+    await debtPage.clickSaveButton();
+    const successMessage = await debtPage.getToastAddSuccess();
+    expect(successMessage).toContain("Thêm thành công");
+  });
+
+  test("value already exists Test", async ({ page }) => {
+    await loginPage.login(Config.admin_username, Config.admin_password);
+    await homePage.clickSalary();
+    await debtPage.clickDebtButton();
+    await debtPage.clickAddButton();
+    await debtPage.fillName("BAT810-Nguyễn Văn Minh");
+    await debtPage.fillAmount("1000000");
+    await debtPage.fillNote("value already exists test");
+    await debtPage.clickSaveButton();
+    const successMessage = await debtPage.getToastAddSuccess();
+    expect(successMessage).toContain("Thêm thành công");
+  });
+
+  test("edit debt Test", async ({ page }) => {
+    await loginPage.login(Config.admin_username, Config.admin_password);
+    await homePage.clickSalary();
+    await debtPage.clickDebtButton();
+    await debtPage.clickAddButton();
+    await debtPage.fillName("BAT810-Nguyễn Văn Minh");
+    await debtPage.fillAmount("1000000");
+    await debtPage.fillNote("add debt test for edit");
+    await debtPage.clickSaveButton();
+    const successMessage = await debtPage.getToastAddSuccess();
+    expect(successMessage).toContain("Thêm thành công");
+    await debtPage.clickEditButton();
+    await debtPage.clickSaveButton();
+    const editSuccessMessage = await debtPage.getToastEditSuccess();
+    expect(editSuccessMessage).toContain("Cập nhật thành công");
+  });
+
+  test("edit debt with empty value Test", async ({ page }) => {
+    await loginPage.login(Config.admin_username, Config.admin_password);
+    await homePage.clickSalary();
+    await debtPage.clickDebtButton();
+    await debtPage.clickAddButton();
+    await debtPage.fillName("BAT810-Nguyễn Văn Minh");
+    await debtPage.fillAmount("1000000");
+    await debtPage.fillNote("add debt test for edit");
+    await debtPage.clickSaveButton();
+    const successMessage = await debtPage.getToastAddSuccess();
+    expect(successMessage).toContain("Thêm thành công");
+    await debtPage.clickEditButton();
+    await debtPage.inputAmount.fill("");
+    await debtPage.inputNote.fill("");
+    await debtPage.clickSaveButton();
+    await debtPage.expectFillAmountError();
+    await debtPage.expectFillNoteError();
+  });
+
+  test("edit debt with valid value Test", async ({ page }) => {
+    await loginPage.login(Config.admin_username, Config.admin_password);
+    await homePage.clickSalary();
+    await debtPage.clickDebtButton();
+    await debtPage.clickAddButton();
+    await debtPage.fillName("BAT810-Nguyễn Văn Minh");
+    await debtPage.fillAmount("1000000");
+    await debtPage.fillNote("add debt test for edit");
+    await debtPage.clickSaveButton();
+    const successMessage = await debtPage.getToastAddSuccess();
+    expect(successMessage).toContain("Thêm thành công");
+    await debtPage.clickEditButton();
+    await debtPage.inputAmount.fill("2000000");
+    await debtPage.inputNote.fill("edit valid value");
+    await debtPage.clickSaveButton();
+    const editSuccessMessage = await debtPage.getToastEditSuccess();
+    expect(editSuccessMessage).toContain("Cập nhật thành công");
+  });
+
+  test("send debt and view in employee account Test", async ({ page }) => {
+    await loginPage.login(Config.admin_username, Config.admin_password);
+    await homePage.clickSalary();
+    await debtPage.clickDebtButton();
+    await debtPage.clickAddButton();
+    await debtPage.fillName("BAT810-Nguyễn Văn Minh");
+    await debtPage.fillAmount("1000000");
+    await debtPage.fillNote("add debt test for send");
+    await debtPage.clickSaveButton();
+    const successMessage = await debtPage.getToastAddSuccess();
+    expect(successMessage).toContain("Thêm thành công");
+    await debtPage.clickActionSendButton();
+    await debtPage.clickYesButton();
+    const sendSuccessMessage = await debtPage.getToastSendSuccess();
+    expect(sendSuccessMessage).toContain("Đã gửi thành công");
+
+    // Login as employee to check the sent debt
+    await debtPage.clickLogoutButton();
+    await debtPage.clickYesButton();
+    await loginPage.login(Config.employee_username, Config.employee_password);
+    await homePage.clickSalary();
+    await debtPage.clickDebtButton();
+
+    // Check if the sent debt is visible
+    const debtRow = page.getByRole("row", {
+      name: "1 BAT810 - Nguyễn Văn Minh 1.000.000 đ add debt test for send Admin Đã gửi",
+      exact: true,
     });
+    await expect(debtRow).toBeVisible();
+  });
 
-    test.afterEach(async ({ page }, testInfo: TestInfo) => {
-        await takeScreenshotOnFailure(page, testInfo);
+  test("send debt and browse in employee account Test", async ({ page }) => {
+    await loginPage.login(Config.admin_username, Config.admin_password);
+    await homePage.clickSalary();
+    await debtPage.clickDebtButton();
+    await debtPage.clickAddButton();
+    await debtPage.fillName("BAT810-Nguyễn Văn Minh");
+    await debtPage.fillAmount("1000000");
+    await debtPage.fillNote("add debt test for send");
+    await debtPage.clickSaveButton();
+    const successMessage = await debtPage.getToastAddSuccess();
+    expect(successMessage).toContain("Thêm thành công");
+    await debtPage.clickActionSendButton();
+    await debtPage.clickYesButton();
+    const sendSuccessMessage = await debtPage.getToastSendSuccess();
+    expect(sendSuccessMessage).toContain("Đã gửi thành công");
+    await debtPage.clickLogoutButton();
+    await debtPage.clickYesButton();
+    await loginPage.login(Config.employee_username, Config.employee_password);
+    await homePage.clickSalary();
+    await debtPage.clickDebtButton();
+    await debtPage.clickActionBrowsedButton();
+    await debtPage.clickYesButton();
+    const browseSuccessMessage = await debtPage.getToastBrowseSuccess();
+    expect(browseSuccessMessage).toContain("Đã gửi thành công");
+    const debtRow = page.getByRole("row", {
+      name: "1 BAT810 - Nguyễn Văn Minh 1.000.000 đ add debt test for send Admin Đã duyệt",
+      exact: true,
     });
+    await expect(debtRow).toBeVisible();
+  });
 
-    test('Example test', async ({ page }) => {
-        await loginPage.login(Config.admin_username, Config.admin_password);
-        await homePage.clickAdmin();
-        
+  test("send debt and refused in employee account Test", async ({ page }) => {
+    await loginPage.login(Config.admin_username, Config.admin_password);
+    await homePage.clickSalary();
+    await debtPage.clickDebtButton();
+    await debtPage.clickAddButton();
+    await debtPage.fillName("BAT810-Nguyễn Văn Minh");
+    await debtPage.fillAmount("1000000");
+    await debtPage.fillNote("add debt test for send");
+    await debtPage.clickSaveButton();
+    const successMessage = await debtPage.getToastAddSuccess();
+    expect(successMessage).toContain("Thêm thành công");
+    await debtPage.clickActionSendButton();
+    await debtPage.clickYesButton();
+    const sendSuccessMessage = await debtPage.getToastSendSuccess();
+    expect(sendSuccessMessage).toContain("Đã gửi thành công");
+    await debtPage.clickLogoutButton();
+    await debtPage.clickYesButton();
+    await loginPage.login(Config.employee_username, Config.employee_password);
+    await homePage.clickSalary();
+    await debtPage.clickDebtButton();
+    await debtPage.clickActionRefusedButton();
+    await debtPage.fillReason("refused debt test");
+    await debtPage.clickYesButton();
+    const refusedSuccessMessage = await debtPage.getToastRefusedSuccess();
+    expect(refusedSuccessMessage).toContain("Từ chối thành công");
+    const debtRow = page.getByRole("row", {
+      name: "1 BAT810 - Nguyễn Văn Minh 1.000.000 đ add debt test for send Admin Từ chối",
+      exact: true,
     });
+    await expect(debtRow).toBeVisible();
+  });
 
+  test("send debt and cancel Test", async ({ page }) => {
+    await loginPage.login(Config.admin_username, Config.admin_password);
+    await homePage.clickSalary();
+    await debtPage.clickDebtButton();
+    await debtPage.clickAddButton();
+    await debtPage.fillName("BAT810-Nguyễn Văn Minh");
+    await debtPage.fillAmount("1000000");
+    await debtPage.fillNote("add debt test for send");
+    await debtPage.clickSaveButton();
+    const successMessage = await debtPage.getToastAddSuccess();
+    expect(successMessage).toContain("Thêm thành công");
+    await debtPage.clickActionSendButton();
+    await debtPage.clickYesButton();
+    const sendSuccessMessage = await debtPage.getToastSendSuccess();
+    expect(sendSuccessMessage).toContain("Đã gửi thành công");
+    await debtPage.clickActionSendCancelButton();
+    await debtPage.fillReason("cancel debt test");
+    await debtPage.clickYesButton();
+    const cancelSuccessMessage = await debtPage.getToastCancelSuccess();
+    expect(cancelSuccessMessage).toContain("Hủy thành công");
+    const debtRow = page.getByRole("row", {
+      name: "1 BAT810 - Nguyễn Văn Minh 1.000.000 đ add debt test for send Admin Hủy",
+      exact: true,
+    });
+    await expect(debtRow).toBeVisible();
+  });
 
+  test("cancel debt Test", async ({ page }) => {
+    await loginPage.login(Config.admin_username, Config.admin_password);
+    await homePage.clickSalary();
+    await debtPage.clickDebtButton();
+    await debtPage.clickAddButton();
+    await debtPage.fillName("BAT810-Nguyễn Văn Minh");
+    await debtPage.fillAmount("1000000");
+    await debtPage.fillNote("add debt test for cancel");
+    await debtPage.clickSaveButton();
+    const successMessage = await debtPage.getToastAddSuccess();
+    expect(successMessage).toContain("Thêm thành công");
+    await debtPage.clickActionCancelButton();
+    await debtPage.fillReason("cancel debt test");
+    await debtPage.clickYesButton();
+    const cancelSuccessMessage = await debtPage.getToastCancelSuccess();
+    expect(cancelSuccessMessage).toContain("Hủy thành công");
+  });
 
+  test("cancel debt with empty reason Test", async ({ page }) => {
+    await loginPage.login(Config.admin_username, Config.admin_password);
+    await homePage.clickSalary();
+    await debtPage.clickDebtButton();
+    await debtPage.clickAddButton();
+    await debtPage.fillName("BAT810-Nguyễn Văn Minh");
+    await debtPage.fillAmount("1000000");
+    await debtPage.fillNote("add debt test for cancel");
+    await debtPage.clickSaveButton();
+    const successMessage = await debtPage.getToastAddSuccess();
+    expect(successMessage).toContain("Thêm thành công");
+    await debtPage.clickActionCancelButton();
+    await debtPage.fillReason("");
+    await debtPage.clickYesButton();
+    await debtPage.expectFillReasonError();
+  });
+
+  
 });
