0e5aec9f1d3e61eca35905dd9404059690b276c7
MinhNguyenETH <minhnguyen.eth@gmail.com>
2026-01-17 09:47:57 +0700

    re run job

--- playwright.config.ts
+++ playwright.config.ts
@@ -18,11 +18,11 @@ export default defineConfig({
 
   use: {
 
-    headless: false, // true run in headless mode. false run with browser window opened.
-    viewport: null,
+    // headless: false, // true run in headless mode. false run with browser window opened.
+    // viewport: null,
 
-    // headless: isHeadless,
-    // viewport: isHeadless ? { width: 1920, height: 1080 } : null,
+    headless: isHeadless,
+    viewport: isHeadless ? { width: 1920, height: 1080 } : null,
     launchOptions: {
       args: ['--start-maximized',],
     },
